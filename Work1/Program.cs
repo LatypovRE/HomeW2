ce00c5b91244d24c2c4410199caaedec975696a5
Radel <latyp47@mail.ru>
2022-05-15 18:49:49 +0300

    Первое задание

---
+++ Work1/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int rundomeNumber = new Random ().Next(100,1000);
+Console.WriteLine(rundomeNumber);
+int numberTwo = (rundomeNumber / 10) % 10;
+Console.WriteLine($"{numberTwo}");
